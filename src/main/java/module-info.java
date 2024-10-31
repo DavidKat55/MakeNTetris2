f7205c3aacbc8de3d2b417bbdcf0ecaeb97e6e0d
DavidKat55 <154361423+DavidKat55@users.noreply.github.com>
2024-10-31 20:06:28 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.makentetris2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.makentetris2 to javafx.fxml;
+    exports org.example.makentetris2;
+}
